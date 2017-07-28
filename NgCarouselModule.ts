2fb8f039cd7965f9a46e880e790140f66a2a7de4
defangit <9065492@qq.com>
2017-07-28 20:17:59 +0800

    ng module

--- NgCarouselModule.ts
+++ NgCarouselModule.ts
@@ -1,9 +1,11 @@
-import {NgModule} from "@angular/core";
+import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
 import {NgCarouselComponent} from "./NgCarouselComponent";
 import {NgCarouselItemComponent} from "./NgCarouselItemComponent";
 
 @NgModule({
-    declarations:[NgCarouselComponent,NgCarouselItemComponent]
+    declarations:[NgCarouselComponent,NgCarouselItemComponent],
+    exports:[NgCarouselComponent,NgCarouselItemComponent],
+    schemas:[NO_ERRORS_SCHEMA]
 })
 export class NgCarouselModule{
 

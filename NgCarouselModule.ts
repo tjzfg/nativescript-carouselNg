7b4821b206e00e211642f6c129d115e90c9395a3
defangit <9065492@qq.com>
2017-08-13 02:00:31 +0800

    fix:add CommonModule to imports

--- NgCarouselModule.ts
+++ NgCarouselModule.ts
@@ -2,8 +2,10 @@ import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
 import {NgCarouselComponent} from "./NgCarouselComponent";
 import {NgCarouselItemComponent} from "./NgCarouselItemComponent";
 import {NgCarouselImagesComponent} from "./NgCarouselImagesComponent";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
+    imports:[CommonModule],
     declarations:[NgCarouselComponent,NgCarouselItemComponent,NgCarouselImagesComponent],
     exports:[NgCarouselComponent,NgCarouselItemComponent,NgCarouselImagesComponent],
     schemas:[NO_ERRORS_SCHEMA]
